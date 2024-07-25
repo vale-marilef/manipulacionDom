e6e2edf7746934e1e5f59ce3547c9a9eb969aa99
vale-marilef <97927229+vale-marilef@users.noreply.github.com>
2024-07-25 15:44:57 -0300

    Add files via upload

---
+++ selectoresDOM.js
@@ -0,0 +1,13 @@
+// SELECTORES INDIVIDUALES
+// getElementById(): Selecciona el elemento por su atrubuto ID
+// querySelector(): Selecciona el elemento con un selector CSS, devolviéndo el primer elemento que coincida
+
+// SELECTORES MÚLTIPLES
+// getElementsByClassName(): Selecciona todos los elementos que tengan la clase especificada
+// getElementsByTagName(): Selecciona todos los elementos con la etiqueta especificada
+// querySelectorAll(): Selecciona todos los elementos con el selector CSS especificado
+
+// ---------------------------------------------------------------------------------
+
+
+
